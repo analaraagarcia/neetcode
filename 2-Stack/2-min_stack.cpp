3a4a8f6d6337f858c9b906d2a69a958720f27306
Ana Lara <analaraagarcia@hotmail.com>
2026-01-28 23:10:24 -0300

    new problems

---
+++ 2-Stack/2-min_stack.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class MinStack {
+    stack<int> mainStack;
+    stack<int> minStack;
+    
+    public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+
+        if (minStack.empty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(min(val, minStack.top()));
+        }
+    }
+    
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
