8b6ca4d649d92db72ca86b19fbccece2c54feab4
Ana Lara <analaraagarcia@hotmail.com>
2026-02-08 12:01:46 -0300

    new problems

--- 2-Stack/3-eval_rpn.cpp
+++ 2-Stack/3-eval_rpn.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <stack>
+#include <algorithm>
 using namespace std;
 
 int evalRPN(vector<string>& tokens) {
-        
+        stack<int> nums;
+
+        for (string& t : tokens) {
+            if (t == "+" || t == "-" || t == "*" || t == "/") {
+                int val2 = nums.top(); 
+                nums.pop();
+
+                int val1 = nums.top(); 
+                nums.pop();
+
+                if (t == "+") {
+                    nums.push(val1 + val2);
+                } else if (t == "-") {
+                    nums.push(val1 - val2);
+                } else if (t == "*") {
+                    nums.push(val1 * val2);
+                } else if (t == "/") {
+                    nums.push(val1 / val2);
+                }
+            } else {
+                nums.push(stoi(t));
+            }
+        }
+        return nums.top();
 }
 
 int main() {
