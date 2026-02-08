1911985b1add6dd4c3fdc2fc7291553c73705c0a
Ana Lara <analaraagarcia@hotmail.com>
2026-02-08 12:09:23 -0300

    changes

--- 2-Stack/3-eval_rpn.cpp
+++ 2-Stack/3-eval_rpn.cpp
@@ -6,30 +6,30 @@
 using namespace std;
 
 int evalRPN(vector<string>& tokens) {
-        stack<int> nums;
+    stack<int> nums;
 
-        for (string& t : tokens) {
-            if (t == "+" || t == "-" || t == "*" || t == "/") {
-                int val2 = nums.top(); 
-                nums.pop();
+    for (string& t : tokens) {
+        if (t == "+" || t == "-" || t == "*" || t == "/") {
+            int val2 = nums.top(); 
+            nums.pop();
 
-                int val1 = nums.top(); 
-                nums.pop();
+            int val1 = nums.top(); 
+            nums.pop();
 
-                if (t == "+") {
-                    nums.push(val1 + val2);
-                } else if (t == "-") {
-                    nums.push(val1 - val2);
-                } else if (t == "*") {
-                    nums.push(val1 * val2);
-                } else if (t == "/") {
-                    nums.push(val1 / val2);
-                }
-            } else {
-                nums.push(stoi(t));
+            if (t == "+") {
+                nums.push(val1 + val2);
+            } else if (t == "-") {
+                nums.push(val1 - val2);
+            } else if (t == "*") {
+                nums.push(val1 * val2);
+            } else if (t == "/") {
+                nums.push(val1 / val2);
             }
+        } else {
+            nums.push(stoi(t));
         }
-        return nums.top();
+    }
+    return nums.top();
 }
 
 int main() {
