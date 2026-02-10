1d915682aad425d40a4a6a449c97304fdd42f8d5
Ana Lara <analaraagarcia@hotmail.com>
2026-02-09 22:41:47 -0300

    new problems

---
+++ 3-Two Pointers/1-valid_palindrome.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    string frase;
+    frase.reserve(s.size());
+
+    for (char& c : s) {
+        if (isalnum(c)) {
+            frase.push_back(tolower(c));
+        }
+    }
+
+    int i = 0;
+    int j = frase.size() - 1;
+    while (i < j) {
+        if (frase[i] != frase[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+int main() {
+    string s = "A man, a plan, a canal: Panama";
+    cout << isPalindrome(s);
+
+    return 0;
+}
