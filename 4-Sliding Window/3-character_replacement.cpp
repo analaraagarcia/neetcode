834c7d612400fd13477c8eaec8ae8e77f5b5a975
Ana Lara <analaraagarcia@hotmail.com>
2026-02-19 15:11:37 -0300

    new problems

---
+++ 4-Sliding Window/3-character_replacement.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int characterReplacement(string s, int k) {
+    int res = 0;
+    int len = s.size();
+    unordered_map<char, int> count;
+    int left = 0;
+    int maxFreq = 0;
+    
+    for (int right = 0; right < len; right++) {
+        count[s[right]]++;
+        maxFreq = max(maxFreq, count[s[right]]);
+
+        while ((right - left + 1) - maxFreq > k) {
+            count[s[left]]--;
+            left++;
+        }
+        res = max(res, right - left + 1);
+    }
+    return res;
+}
+
+int main() {
+    string s = "ABAB";
+    
+    cout << characterReplacement(s, 2);
+
+    return 0;
+}
