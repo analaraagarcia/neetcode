0539932a5ae03a77ed4883e94205f97c24d1f364
Ana Lara <analaraagarcia@hotmail.com>
2026-02-23 09:18:45 -0300

    new problems

---
+++ 4-permutation_in_string.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// hash
+bool checkInclusion1(string s1, string s2) { // time complexity: O(n*m) , space complexity: O(1)
+    unordered_map<char, int> count1;
+    for (char c : s1) {
+        count1[c]++;
+    }
+
+    int need = count1.size();
+    int len = s2.size();
+    for (int i = 0; i < len; i++) {
+        unordered_map<char, int> count2;
+        int cur = 0;
+        for (int j = i; j < len; j++) {
+            char c = s2[j];
+            count2[c]++;
+
+            if (count1[c] < count2[c]) {
+                break;
+            }
+
+            if (count1[c] == count2[c]) {
+                cur++;
+            }
+
+            if (cur == need) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// sliding window
+bool checkInclusion(string s1, string s2) { // time complexity: O(n) , space complexity: O(1)
+    if (s1.length() > s2.length()) {
+        return false;
+    }
+
+    vector<int> s1Count(26, 0);
+    vector<int> s2Count(26, 0);
+    for (int i = 0; i < s1.length(); i++) {
+        s1Count[s1[i] - 'a']++;
+        s2Count[s2[i] - 'a']++;
+    }
+
+    int matches = 0;
+    for (int i = 0; i < 26; i++) {
+        if (s1Count[i] == s2Count[i]) {
+            matches++;
+        }
+    }
+
+    int l = 0;
+    for (int r = s1.length(); r < s2.length(); r++) {
+        if (matches == 26) {
+            return true;
+        }
+
+        int index = s2[r] - 'a';
+        s2Count[index]++;
+        if (s1Count[index] == s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] + 1 == s2Count[index]) {
+            matches--;
+        }
+
+        index = s2[l] - 'a';
+        s2Count[index]--;
+        if (s1Count[index] == s2Count[index]) {
+            matches++;
+        } else if (s1Count[index] - 1 == s2Count[index]) {
+            matches--;
+        }
+        l++;
+    }
+    return matches == 26;
+}
