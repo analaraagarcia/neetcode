7550c365fab730fb43924ece62c4b6ec41c9be84
Ana Lara <analaraagarcia@hotmail.com>
2026-07-02 11:56:13 -0300

    added new sliding window problem in python

--- PYTHON/4-SlidingWindow/1-buy_sell_stock.py
+++ PYTHON/4-SlidingWindow/1-buy_sell_stock.py
@@ -14,6 +14,17 @@ def maxProfit(prices: List[int]) -> int:
 
     return max_prof
 
+# dp
+# def maxProfit(prices: List[int]) -> int:
+#     max_prof = 0
+#     min_buy = prices[0]
+
+#     for sell in prices:
+#         max_prof = max(max_prof, sell - min_buy)
+#         min_buy = min(min_buy, sell)
+    
+#     return max_prof
+
 if __name__ == "__main__":
     prices = [10, 1, 5, 6, 7, 1]
     print(maxProfit(prices))
