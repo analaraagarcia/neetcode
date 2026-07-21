1838bbeb93001b960a663fd62a5124f6c01364f5
Ana Lara <analaraagarcia@hotmail.com>
2026-07-21 15:10:36 -0300

    added new binary search problem in python

---
+++ PYTHON/5-BinarySearch/5-search_in_array.py
@@ -0,0 +1,27 @@
+from typing import List
+
+def search(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return mid
+        
+        if nums[left] <= nums[mid]:
+            if target > nums[mid] or target < nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        else:
+            if target < nums[mid] or target > nums[right]:
+                right = mid - 1
+            else:
+                left = mid + 1
+    
+    return -1
+
+if __name__ == "__main__":
+    nums = [3, 4, 5, 6, 1, 2]
+    target = 1
+    print(search(nums, target))
