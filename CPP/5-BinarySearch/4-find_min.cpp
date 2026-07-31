6929637b5436f19946f07947a3e742969aa9c261
Ana Lara <analaraagarcia@hotmail.com>
2026-07-31 17:56:34 -0300

    added new binary search problem in C++

---
+++ CPP/5-BinarySearch/4-find_min.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMin(vector<int>& nums) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < nums[right]) {
+            right = mid;
+        } else if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        }
+    }
+
+    return nums[right];
+}
+
+int main() {
+    vector<int> nums = {3, 4, 5, 6, 1, 2};
+    cout << findMin(nums);
+}
