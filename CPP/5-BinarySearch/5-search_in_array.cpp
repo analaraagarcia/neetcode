e0d654b9e3d08074f5ff53ad220f02132ef67e44
Ana Lara <analaraagarcia@hotmail.com>
2026-08-03 20:31:57 -0300

    added new binary search problem in C++

---
+++ CPP/5-BinarySearch/5-search_in_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (target == nums[mid]) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            if (target < nums[mid] || target > nums[right]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {3,4,5,6,1,2};
+    int target = 1;
+    cout << search(nums, target);
+}
